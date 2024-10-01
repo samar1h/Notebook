2530dfb561cd783a7cb5dd9658af959559d42c7c
samar1h <a@gmail.com>
2024-10-01 12:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "Notebook"
+    page.theme_mode = ft.ThemeMode.SYSTEM
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    def btn_onclick(e):
+        print("Here's a secret message :-)")
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.Text("Hello World!"),
+                ft.IconButton("rocket", "red", on_click=btn_onclick),
+            ]
+        )
+    )
 
 
 ft.app(main)
